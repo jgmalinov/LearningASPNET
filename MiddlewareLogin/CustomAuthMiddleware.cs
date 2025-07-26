8d65afb2b944e972f24bd77b4b1b83879bc190d2
jgmalinov <gogotkd24@gmail.com>
2025-07-26 20:30:15 +0300

    Added body parsing logic

--- MiddlewareLogin/CustomAuthMiddleware.cs
+++ MiddlewareLogin/CustomAuthMiddleware.cs
@@ -1,4 +1,7 @@
-﻿namespace MiddlewareLogin
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+
+namespace MiddlewareLogin
 {
     public class CustomAuthMiddleware
     {
@@ -18,54 +21,63 @@
             else if (requestMethod == "POST")
             {
                 List<string> errorMessages = new List<string>();
-                string? username = context.Request.Query["username"];
-                string? password = context.Request.Query["password"];
+                string? password, username;
+                
+                if (context.Request.Query.Count > 0)
+                {
+                    username = context.Request.Query["username"];
+                    password = context.Request.Query["password"];
+                } else
+                {
+                    StreamReader reader = new StreamReader(context.Request.Body);
+                    string body = await reader.ReadToEndAsync();
+
+
+
+                    Dictionary<string, StringValues> queryDict = QueryHelpers.ParseQuery(body);
+
+                    username = queryDict.ContainsKey("username") ? queryDict["username"][0].ToString() : null;
+                    password = queryDict.ContainsKey("password") ? queryDict["password"][0].ToString() : null;
+                }
+
                 bool usernameIsValid = true;
                 bool passwordIsValid = true;
 
-                if (context.Request.Method == "GET")
+                if (username == null)
                 {
-                    context.Response.StatusCode = 200;
+                    errorMessages.Add("Invalid input for username");
                 }
-                else if (context.Request.Method == "POST")
+                else if (username != "admin@example.com")
                 {
-                    if (username == null)
-                    {
-                        errorMessages.Add("Invalid input for username");
-                    }
-                    else if (username != "admin@example.com")
-                    {
-                        usernameIsValid = false;
-                    }
-
-                    if (password == null)
-                    {
-                        errorMessages.Add("Invalid input for password");
-                    }
-                    else if (password != "admin1234")
-                    {
-                        passwordIsValid = false;
-                    }
-
-                    if (errorMessages.Count == 0 && (!passwordIsValid || !usernameIsValid))
-                    {
-                        errorMessages.Add("Invalid Login");
-                    }
-
-                    if (errorMessages.Count > 0)
-                    {
-                        context.Response.StatusCode = 400; // Unauthorized
-                        await context.Response.WriteAsync(string.Join("\n", errorMessages));
-                        return;
-                    }
-                    else
-                    {
-                        context.Response.StatusCode = 200; // OK
-                        await context.Response.WriteAsync("Login successful");
-                        return;
-                    }
+                    usernameIsValid = false;
                 }
-                await _next(context); // Call the next middleware in the pipeline
+
+                if (password == null)
+                {
+                    errorMessages.Add("Invalid input for password");
+                }
+                else if (password != "admin1234")
+                {
+                    passwordIsValid = false;
+                }
+
+                if (errorMessages.Count == 0 && (!passwordIsValid || !usernameIsValid))
+                {
+                    errorMessages.Add("Invalid Login");
+                }
+
+                if (errorMessages.Count > 0)
+                {
+                    context.Response.StatusCode = 400; // Unauthorized
+                    await context.Response.WriteAsync(string.Join("\n", errorMessages));
+                    return;
+                }
+                else
+                {
+                    context.Response.StatusCode = 200; // OK
+                    await context.Response.WriteAsync("Login successful");
+                    return;
+                } // Call the next middleware in the pipeline
             }
         }
     }
