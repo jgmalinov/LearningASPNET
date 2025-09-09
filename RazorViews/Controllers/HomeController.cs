a2ff6a062037283ddd24723455a7a60c48d597ad
jgmalinov <gogotkd24@gmail.com>
2025-09-09 14:59:28 +0300

    ViewData, enabled static files, basic styles applied

--- RazorViews/Controllers/HomeController.cs
+++ RazorViews/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RazorViews.Models;
 
 namespace RazorViews.Controllers
 {
@@ -11,6 +12,21 @@ namespace RazorViews.Controllers
             return View();
             // return View("AlternativeViewName");
             // return new ViewResult { ViewName = "AlternativeViewName" };
+        }
+
+        [Route("Proper")]
+        public IActionResult Proper()
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person() { Id = 1, Name = "Alice", Gender=Gender.Female, DateOfBirth = Convert.ToDateTime("2000-12-12")},
+                new Person() { Id = 2, Name = "Bob", Gender=Gender.Male, DateOfBirth = Convert.ToDateTime("1956-12-24")},
+                new Person() { Id = 3, Name = "Charlie", Gender=Gender.Male }
+            };
+            ViewData["Title"] = "Proper View";
+            ViewData["People"] = people;
+
+            return View();
         }
     }
 }
