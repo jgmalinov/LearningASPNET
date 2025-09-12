dd3cd765e9e1d0b63c5dec3c88d4af792d607653
jgmalinov <gogotkd24@gmail.com>
2025-09-12 11:16:58 +0300

    Strongly-typed views with a single instance or IEnumerable of the model. ViewBox as an abstraction layer over ViewData

--- RazorViews/Controllers/HomeController.cs
+++ RazorViews/Controllers/HomeController.cs
@@ -5,6 +5,13 @@ namespace RazorViews.Controllers
 {
     public class HomeController : Controller
     {
+        List<Person> People { get; set; } = new List<Person>()
+        {
+                new Person() { Id = 1, Name = "Alice", Gender=Gender.Female, DateOfBirth = Convert.ToDateTime("2000-12-12")},
+                new Person() { Id = 2, Name = "Bob", Gender=Gender.Male, DateOfBirth = Convert.ToDateTime("1956-12-24")},
+                new Person() { Id = 3, Name = "Charlie", Gender=Gender.Male }
+        };
+
         [Route("Home")]
         [Route("")]
         public IActionResult Index()
@@ -17,16 +24,30 @@ namespace RazorViews.Controllers
         [Route("Proper")]
         public IActionResult Proper()
         {
-            List<Person> people = new List<Person>()
-            {
-                new Person() { Id = 1, Name = "Alice", Gender=Gender.Female, DateOfBirth = Convert.ToDateTime("2000-12-12")},
-                new Person() { Id = 2, Name = "Bob", Gender=Gender.Male, DateOfBirth = Convert.ToDateTime("1956-12-24")},
-                new Person() { Id = 3, Name = "Charlie", Gender=Gender.Male }
-            };
+
             ViewData["Title"] = "Proper View";
-            ViewData["People"] = people;
+            //ViewBag.People = people;
+            ViewData["People"] = People;
+
 
             return View();
+        }
+
+        [Route("StronglyTyped")]
+        public IActionResult StronglyTyped()
+        {
+            return View(People);
+        }
+
+        [Route("Person/{name:alpha}")]
+        public IActionResult Person(string name)
+        {
+            var person = People.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
         }
     }
 }
