b56cae543abe0057fe5f5fffa3e59f276590ba2e
jgmalinov <gogotkd24@gmail.com>
2025-09-17 12:25:38 +0300

    Razor View basics and model binding and validation projects complete

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public List<CityWeather> CityWeatherList { get; set; } = new List<CityWeather>()
+        {
+            new CityWeather() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = DateTime.Now, TemperatureFahrenheit = 32 },
+            new CityWeather() { CityUniqueCode = "LAX", CityName = "Los Angeles", DateAndTime = DateTime.Now, TemperatureFahrenheit = 85 },
+            new CityWeather() { CityUniqueCode = "CHI", CityName = "Chicago", DateAndTime = DateTime.Now, TemperatureFahrenheit = 50 },
+            new CityWeather() { CityUniqueCode = "HOU", CityName = "Houston", DateAndTime = DateTime.Now, TemperatureFahrenheit = 90 },
+            new CityWeather() { CityUniqueCode = "PHX", CityName = "Phoenix", DateAndTime = DateTime.Now, TemperatureFahrenheit = 100 }
+        };
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(CityWeatherList);
+        }
+
+        [Route("/{cityCode:alpha}")]
+        public IActionResult Details(string cityCode)
+        {
+            CityWeather cityWeather = CityWeatherList.FirstOrDefault(cw => cw.CityUniqueCode.Equals(cityCode, StringComparison.OrdinalIgnoreCase));
+            if (cityWeather == null)
+            {
+                return NotFound("City not found");
+            }
+            return View(cityWeather);
+        }
+    }
+}
