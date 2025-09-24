434e8c76e5bbc8084abb82dfa651ec2019363280
jgmalinov <gogotkd24@gmail.com>
2025-09-24 11:33:55 +0300

    Layouts, nested layouts, conditional layouts, viewimports and viewstart

---
+++ Views-Part2/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Views_Part2.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("/products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/products/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("/products/search/{id:int?}")]
+        public IActionResult Search(int? id)
+        {
+            ViewBag.ProductId = id;
+            return View();
+        }
+    }
+}
